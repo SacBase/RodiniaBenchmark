d85adbec8f5e5723bae0c091c2faa4d7b6955f4c
Max Baird <mmb1@hw.ac.uk>
2017-11-25 15:53:12 +0000

    Added comments and removed a flag when compiling the backprop benchmark

--- build-benchmark.cmake
+++ build-benchmark.cmake
@@ -1,5 +1,20 @@
+# This file is a macro that can be used to build the
+# sac rodinia benchmarks.
+
+# Assumptions:
+#   *SAC_SRC has been previously defined.  
+
+# This macro builds rodinina benchmarks with the specified flags (if any).
+#
+#
+# Arguments:
+#       sac2c_flags:
+#               Flags that sac2c needs to build the program.
 MACRO (BUILD_BENCHMARK sac2c_flags)
-   
+    # Sanity check. SAC_SRC 
+    # should hold the list of sac files needed to compile
+    # the current benchmark and should be set in the CMakeLists.txt
+    # file located in the root of each benchmark.
     IF(NOT SAC_SRC) 
         MESSAGE(FATAL_ERROR, "The sac file dependencies variable is not set")
     ENDIF()
@@ -15,11 +30,10 @@ MACRO (BUILD_BENCHMARK sac2c_flags)
     
       GET_FILENAME_COMPONENT (dst ${name} NAME_WE)
     
-      SET (dst "${CMAKE_CURRENT_BINARY_DIR}/${dir}/${dst}.out")
-    
       FILE (MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${dir}")
 
-      SAC2C_COMPILE_PROG(${name} ${name}.out "${SAC_SRC}" "${sac2c_flags}")
+      #SAC2C_COMPILE_PROG is a macro located in cmake-common/build-sac2c-program.cmake
+      SAC2C_COMPILE_PROG(${name} ${dst}.out "${SAC_SRC}" "${sac2c_flags}")
     
     ENDFOREACH(name)
 ENDMACRO()
